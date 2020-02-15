d07c2b594ef1d70ffbd61a064a5065e0cf8ff02c
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2020-02-15 13:38:31 +0200

    Create skeleton of project. Add ModelMapper and Swagger config, database and resource mapping constants, application properties for dev and prod, dependencies

---
+++ src/main/java/task/config/ModelMapperConfig.java
@@ -0,0 +1,26 @@
+package task.config;
+
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@AllArgsConstructor
+@Profile({"dev", "prod"})
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+            .setMatchingStrategy(MatchingStrategies.STRICT)
+            .setFieldMatchingEnabled(true)
+            .setSkipNullEnabled(true)
+            .setFieldAccessLevel(AccessLevel.PRIVATE);
+        return mapper;
+    }
+}
---
+++ src/main/java/task/constants/ResourceMapping.java
@@ -0,0 +1,6 @@
+package task.constants;
+
+public final class ResourceMapping {
+    public static final String USER = "user";
+    public static final String ARTICLES = "articles";
+}
