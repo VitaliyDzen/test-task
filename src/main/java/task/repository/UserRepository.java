93798e7f6313897287cba4e34cb0a7185d23aabe
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2020-02-16 22:55:39 +0200

    Update repository for security. Add method findByEmail

--- src/main/java/task/repository/UserRepository.java
+++ src/main/java/task/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package task.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,9 @@ import task.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
     List<User> findUsersByAgeGreaterThan(Integer age);
 
     @Query(value = "SELECT DISTINCT u.name FROM User u WHERE SIZE(u.articles) > ?1")
