cc255abffd8b85be106cc4f7c910e0f0104ec142
qqqLeo <44520458+qqqLeo@users.noreply.github.com>
2020-10-10 09:40:17 +0100

    Change UserRepository
    Change UserRepository. Add new updates

--- src/main/java/task/repository/UserRepository.java
+++ src/main/java/task/repository/UserRepository.java
@@ -11,13 +11,32 @@ import task.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    
+     <S extends T> S save(S entity);
+                                                                                                                    
+    T findOne(ID primaryKey);
+                                                                                                                    
+    Iterable<T> findAll();
+
+    Long count();
+                                                                                                                       
+    void delete(T entity);
+                                                                                                                     
+    boolean exists(ID primaryKey);
+    
     Optional<User> findByEmail(String email);
 
+     List<Article> findAllByColor(Color color);
+    
     List<User> findUsersByAgeGreaterThan(Integer age);
 
     @Query(value = "SELECT DISTINCT u.name FROM User u WHERE SIZE(u.articles) > ?1")
     List<String> findUniqueUserNamesByArticlesCountGreaterThan(Integer count);
 
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+    
     @Modifying
     @Query(value = "UPDATE User SET refreshTokenKey=:refreshTokenKey WHERE id=:id")
     void updateUserRefreshToken(String refreshTokenKey, Long id);
