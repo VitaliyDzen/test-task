86713e8674e1911bd0a74b776806459df24fc656
admin303 <32466001+admin303@users.noreply.github.com>
2020-10-30 13:50:57 +0000

    Change ModelMapperConfig
    Change ModelMapperConfig

--- src/main/java/task/config/ModelMapperConfig.java
+++ src/main/java/task/config/ModelMapperConfig.java
@@ -12,15 +12,33 @@ import org.springframework.context.annotation.Profile;
 @AllArgsConstructor
 @Profile({"dev", "prod"})
 public class ModelMapperConfig {
+    
+    
 
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration()
-            .setMatchingStrategy(MatchingStrategies.STRICT)
-            .setFieldMatchingEnabled(true)
-            .setSkipNullEnabled(true)
-            .setFieldAccessLevel(AccessLevel.PRIVATE);
-        return mapper;
+    Account account = Account.builder().id("123456")
+            .name("testAccount")
+            .email("test@staffjoy.net")
+            .memberSince(Instant.now())
+            .confirmedAndActive(true)
+            .photoUrl("https://staffjoy.xyz/photo/test.png")
+            .phoneNumber("18001801266")
+            .support(false)
+            .build();
+
+    AccountDto accountDto = modelMapper.map(account, AccountDto.class);
+    validateAccount(accountDto, account);
+
+    Account account1 = modelMapper.map(accountDto, Account.class);
+    validateAccount(accountDto, account1);
+    
+    
+      @Override
+    protected void configure(){
+        using(toUppercase)
+            .map(source.getProperty())
+            .setOther(null);
+    }
+    
+    
     }
 }
