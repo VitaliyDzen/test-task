ff8080a26c07c90fe393bf1f1956be33d115d2dc
LEOsmb <44555690+LEOsmb@users.noreply.github.com>
2021-10-24 20:31:33 +0300

    Create DepartmentRepository.java

---
+++ src/main/java/task/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByName(String name);
+
+    List<Department> findByInstituteName(String institute);
+}
